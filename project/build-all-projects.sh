6a98abbe06df956be3bc68ec44b51790765c26be
maroliva <marcoo195@hotmail.it>
2018-05-28 15:11:51 +0200

    gradle cache

--- project/build-all-projects.sh
+++ project/build-all-projects.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 #Build eureka
-gradle build -b eureka-server/build.gradle
+gradle build -b eureka-server/build.gradle --build-cache
 rc=$?; if [[ $rc != 0 ]]; then echo "Build eureka-server failed"; exit $rc; fi
 
 #Build word-service
-gradle build -b word-service/build.gradle
+gradle build -b word-service/build.gradle --build-cache
 rc=$?; if [[ $rc != 0 ]]; then echo "Build word-service failed"; exit $rc; fi
 
 
 #Build sentence-service
-gradle build -b sentence-service/build.gradle
+gradle build -b sentence-service/build.gradle --build-cache
 rc=$?; if [[ $rc != 0 ]]; then echo "Build sentence-service failed"; exit $rc; fi
 
 
 #Build zuul
-gradle build -b zuul/build.gradle
+gradle build -b zuul/build.gradle --build-cache
 rc=$?; if [[ $rc != 0 ]]; then echo "Build zuul failed"; exit $rc; fi
 
 
