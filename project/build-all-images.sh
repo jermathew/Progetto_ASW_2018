6a59610f6d50f032cabf8ea05278e247f2a01fb4
Vincenzo Martello <vmartello.95@gmail.com>
2018-05-28 16:32:52 +0200

    modificato script per caricare immagini su dockerhub

--- project/build-all-images.sh
+++ project/build-all-images.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-docker build --rm -t eureka-server-img ./eureka-server 
-docker build --rm -t word-img ./word-service 
-docker build --rm -t sentence-img ./sentence-service
-docker build --rm -t zuul-img ./zuul
+docker build --rm -t vmartello95/eureka-server-img ./eureka-server 
+docker build --rm -t vmartello95/word-img ./word-service 
+docker build --rm -t vmartello95/sentence-img ./sentence-service
+docker build --rm -t vmartello95/zuul-img ./zuul
 
+docker push vmartello95/eureka-server-img
 
 
 
