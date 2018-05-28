0dedd26f780cf431480fcff558ace30e736c088e
maroliva <marcoo195@hotmail.it>
2018-05-28 14:46:06 +0200

    test gradle cache

--- project/build-all-projects.sh
+++ project/build-all-projects.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-gradle build -b eureka-server/build.gradle 
-gradle build -b word-service/build.gradle 
-gradle build -b sentence-service/build.gradle 
-gradle build -b zuul/build.gradle 
+gradle build -b eureka-server/build.gradle
+gradle build -b word-service/build.gradle
+gradle build -b sentence-service/build.gradle
+gradle build -b zuul/build.gradle
 
 
 
