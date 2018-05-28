7cbe524eda7aab90f6d02ab3cdc875df28eef315
maroliva <marcoo195@hotmail.it>
2018-05-28 15:03:04 +0200

    build-all-project.sh ritorna l'errore anziche 0

--- project/build-all-projects.sh
+++ project/build-all-projects.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+#Build eureka
 gradle build -b eureka-server/build.gradle
+rc=$?; if [[ $rc != 0 ]]; then echo "Build eureka-server failed"; exit $rc; fi
+
+#Build word-service
 gradle build -b word-service/build.gradle
+rc=$?; if [[ $rc != 0 ]]; then echo "Build word-service failed"; exit $rc; fi
+
+
+#Build sentence-service
 gradle build -b sentence-service/build.gradle
+rc=$?; if [[ $rc != 0 ]]; then echo "Build sentence-service failed"; exit $rc; fi
+
+
+#Build zuul
 gradle build -b zuul/build.gradle
+rc=$?; if [[ $rc != 0 ]]; then echo "Build zuul failed"; exit $rc; fi
 
 
 
