7f8e3cb6af51874ff3ff94ceb0c4d5391d27cb43
simpe <simpe@Simone-Ultrabook>
2018-06-06 15:53:24 +0200

    SentenceControllerTests funzionante. Da correggere WordControllerTests

--- project/sentence-service/src/test/java/asw/springcloud/sentence/SentenceControllerTests.java
+++ project/sentence-service/src/test/java/asw/springcloud/sentence/SentenceControllerTests.java
@@ -5,53 +5,47 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.Ignore;
-
-/*
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 import org.mockito.Mockito;
 
-import asw.springcloud.sentence.wordclient.*;
-import asw.springcloud.sentence.SentenceServiceImpl; 
-import asw.springcloud.sentence.domain.Word; */
+import asw.springcloud.sentence.word.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Ignore
 public class SentenceControllerTests {
+	//Class under test:
+	WordServiceHystrixImpl service;
 
 	@Test
 	public void contextLoads() {
 	}
-	/*
-	//	Class under test:
-	SentenceServiceImpl service;
-	
+
 	@Before
 	public void setup() {
 
-		service = new SentenceServiceImpl();
-		
+		service = new WordServiceHystrixImpl();
+
 		//	Establish Mock Dependencies:
 		SubjectClient subject = Mockito.mock(SubjectClient.class);
 		VerbClient verb = Mockito.mock(VerbClient.class);
 		ObjectClient object = Mockito.mock(ObjectClient.class);
 
-		service.setSubjectService(subject);
-		service.setVerbService(verb);
-		service.setObjectService(object);
-		
+		service.setSubject(subject);
+		service.setVerb(verb);
+		service.setObject(object);
+
 		//	Describe Mock Behaviors:
-		Mockito.when(subject.getWord()).thenReturn(new Word("1"));
-		Mockito.when(verb.getWord()).thenReturn(new Word("2"));
-		Mockito.when(object.getWord()).thenReturn(new Word("3"));
+		Mockito.when(subject.getWord()).thenReturn("1");
+		Mockito.when(verb.getWord()).thenReturn("2");
+		Mockito.when(object.getWord()).thenReturn("3");
 	}
-	
+
 	@Test
 	public void test() {
 		//	We should get the sentence built in the correct order:
 		Assert.assertEquals("1 2 3.", service.buildSentence());
 	}
-*/
 }
