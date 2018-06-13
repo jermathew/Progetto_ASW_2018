668cbce3f9d24159fbf5f6e0a0f28ca580c067ca
Jerin George Mathew <jerin.edassery95@gmail.com>
2018-06-13 14:15:56 +0200

    update deploy script

--- project/deploy.sh
+++ project/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 scp -o "StrictHostKeyChecking no" -i asw_key.pem project/restart-service.sh $AWS_USER@$AWS_HOSTNAME:$AWS_DEPLOY_PATH
+scp -o "StrictHostKeyChecking no" -i asw_key.pem project/docker-compose.yml $AWS_USER@$AWS_HOSTNAME:$AWS_DEPLOY_PATH
 ssh -o "StrictHostKeyChecking no" -i asw_key.pem $AWS_USER@$AWS_HOSTNAME 'bash restart-service.sh'
