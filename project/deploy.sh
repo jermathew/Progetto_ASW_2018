6d9ada9792ac4f3b8dc34e53052a7d09eb76ee92
Jerin George Mathew <jerin.edassery95@gmail.com>
2018-06-13 15:08:58 +0200

    add end-to-end test

--- project/deploy.sh
+++ project/deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-scp -o "StrictHostKeyChecking no" -i asw_key.pem project/restart-service.sh $AWS_USER@$AWS_HOSTNAME:$AWS_DEPLOY_PATH
-scp -o "StrictHostKeyChecking no" -i asw_key.pem project/docker-compose.yml $AWS_USER@$AWS_HOSTNAME:$AWS_DEPLOY_PATH
-ssh -o "StrictHostKeyChecking no" -i asw_key.pem $AWS_USER@$AWS_HOSTNAME 'bash restart-service.sh'
+#scp -o "StrictHostKeyChecking no" -i asw_key.pem project/restart-service.sh $AWS_USER@$AWS_HOSTNAME:$AWS_DEPLOY_PATH
+#scp -o "StrictHostKeyChecking no" -i asw_key.pem project/docker-compose.yml $AWS_USER@$AWS_HOSTNAME:$AWS_DEPLOY_PATH
+#ssh -o "StrictHostKeyChecking no" -i asw_key.pem $AWS_USER@$AWS_HOSTNAME 'bash restart-service.sh'
+
+#Aspetto 5 minuti e controllo
+#sleep 300
+curl -s "http://${AWS_HOSTNAME}:8080/sentence" | awk '{n=split($0, array, " "); if (n==3) {exit 0;} else {exit 1;}}'
+
